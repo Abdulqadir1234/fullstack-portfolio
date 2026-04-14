c4efd6e2eba3a55bbe97a7d269b3e7b94798ade0
Abdulqadir1234 <Abdulqadir1234@users.noreply.github.com>
2026-04-14 09:46:11 -0700

    updated

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,7 @@ use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PageController::class, 'index'])->name('home');
+Route::get('/resume', function () {
+    return view('app');
+})->name('resume');
 Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
